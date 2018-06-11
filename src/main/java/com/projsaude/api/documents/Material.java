17213952494dd03024b172515e7fa651d2403bd6
Henrique Araujo <henriarasilva@gmail.com>
2018-06-11 14:16:28 -0300

    Colocando coisas no API documents no Material

--- src/main/java/com/projsaude/api/documents/Material.java
+++ src/main/java/com/projsaude/api/documents/Material.java
@@ -1,8 +1,55 @@
 package com.projsaude.api.documents;
 
-
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
 
 public class Material {
 
+	@Id
+	private Integer id;
+	private String nome;
+	private String marca;
+	private String tamanho;
+	
+	public Material () {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+	@NotEmpty(message = "O nome não pode ser vazio")
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@NotEmpty(message = "A marca não pode ser vazia")
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@NotEmpty(message = "O tamanho não pode ser vazia")
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	
 	
 }
