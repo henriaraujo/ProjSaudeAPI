7cd15ab791cb4e784d9b229889abe8551f769e28
Henrique Araujo <henriarasilva@gmail.com>
2018-06-11 09:44:55 -0300

    Projeto Criado

---
+++ src/main/java/com/projsaude/api/ProjSaudeApiApplication.java
@@ -0,0 +1,12 @@
+package com.projsaude.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSaudeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSaudeApiApplication.class, args);
+	}
+}
