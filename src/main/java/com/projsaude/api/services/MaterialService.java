ce7db72c51df7026d9c4733287e8efb6efdcd8e2
Henrique Araujo <henriarasilva@gmail.com>
2018-06-11 14:33:57 -0300

    Material Controller

--- src/main/java/com/projsaude/api/services/MaterialService.java
+++ src/main/java/com/projsaude/api/services/MaterialService.java
@@ -1,5 +1,14 @@
 package com.projsaude.api.services;
 
+import java.util.List;
+
+import com.projsaude.api.documents.Material;
+
 public interface MaterialService {
 
+	List<Material> listarTodos();
+	Material listarPoId (String id);
+	Material cadastrar (Material material);
+	Material atualizar (Material material);
+	void remover (String id);
 }
