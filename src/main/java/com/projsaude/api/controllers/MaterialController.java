4a1a304a2ddf47131ad173b00c22b739fdd35ad2
Henrique Araujo <henriarasilva@gmail.com>
2018-06-11 15:08:05 -0300

    Material Basico implementado

--- src/main/java/com/projsaude/api/controllers/MaterialController.java
+++ src/main/java/com/projsaude/api/controllers/MaterialController.java
@@ -1,5 +1,73 @@
 package com.projsaude.api.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projsaude.api.documents.Material;
+import com.projsaude.api.responses.Response;
+import com.projsaude.api.services.MaterialService;
+
+
+@RestController
+@RequestMapping(path = "/api/materiais")
 public class MaterialController {
 
+	@Autowired
+	private MaterialService materialService;
+	
+	@GetMapping
+	public ResponseEntity<Response<List<Material>>> listarTodos()
+	{
+		return ResponseEntity.ok(new Response<List<Material>>(this.materialService.listarTodos()));
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Response<Material>> listarPorId(@PathVariable(name = "id") String id)
+	{
+		return ResponseEntity.ok(new Response<Material>(this.materialService.listarPorId(id)));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Response<Material>> cadastrar(@Valid @RequestBody Material material, BindingResult result)
+	{
+		if(result.hasErrors()) {
+			List<String> erros = new ArrayList<String>();
+			result.getAllErrors().forEach(erro -> erros.add(erro.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(new Response<Material>(erros));
+		}
+		return ResponseEntity.ok(new Response<Material>(this.materialService.cadastrar(material)));
+	}
+	
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<Response<Material>> atualizar(@PathVariable(name = "id") String id,@RequestBody Material material, BindingResult result)
+	{
+		if(result.hasErrors()) {
+			List<String> erros = new ArrayList<String>();
+			result.getAllErrors().forEach(erro -> erros.add(erro.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(new Response<Material>(erros));
+		}
+		material.setId(id);
+		return ResponseEntity.ok(new Response<Material>(this.materialService.atualizar(material)));
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Response<Integer>> remover(@PathVariable(name = "id") String id)
+	{
+		this.materialService.remover(id);
+		return ResponseEntity.ok(new Response<Integer>(1));
+	}
 }
--- src/main/java/com/projsaude/api/services/impl/MaterialServiceImpl.java
+++ src/main/java/com/projsaude/api/services/impl/MaterialServiceImpl.java
@@ -1,7 +1,41 @@
 package com.projsaude.api.services.impl;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.projsaude.api.documents.Material;
+import com.projsaude.api.repositories.MaterialRepository;
 import com.projsaude.api.services.MaterialService;
 
+@Service
 public class MaterialServiceImpl implements MaterialService {
+
+	@Autowired
+	private MaterialRepository materialRepository;
+	
+	@Override
+	public List<Material> listarTodos() {
+		return this.materialRepository.findAll();
+	}
+
+	@Override
+	public Material listarPorId(String id) {
+		return this.materialRepository.findOne(id);
+	}
+
+	@Override
+	public Material cadastrar(Material material) {
+		return this.materialRepository.save(material);
+	}
+
+	@Override
+	public Material atualizar(Material material) {
+		return this.materialRepository.save(material);
+	}
+
+	@Override
+	public void remover(String id) {
+		this.materialRepository.delete(id);
+	}
 
 }
